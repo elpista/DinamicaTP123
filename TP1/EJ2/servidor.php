ad0425a87c95fae5bc8f6f8c5422c25531c1d938
Marco pistagnesi <espadachin2003@gmail.com>
2023-08-18 15:52:05 -0300

    ejercicio 2 actualizado

--- TP1/EJ2/servidor.php
+++ TP1/EJ2/servidor.php
@@ -1,11 +1,13 @@
 <?php
-    $lunes = $_GET["form_lunes"];
-    $martes = $_GET["form_martes"];
-    $miercoles = $_GET["form_miercoles"];
-    $jueves = $_GET["form_jueves"];
-    $viernes = $_GET["form_viernes"];
+    $dias = [];
 
-    $horasPorsemana = ($lunes + $martes + $miercoles + $jueves + $viernes)/5;
+    $dias["lunes"] = $_GET["form_lunes"];
+    $dias["martes"] = $_GET["form_martes"];
+    $dias["miercoles"] = $_GET["form_miercoles"];
+    $dias["jueves"] = $_GET["form_jueves"];
+    $dias["viernes"] = $_GET["form_viernes"];
+
+    $horasPorsemana = ($dias["lunes"] + $dias["martes"] + $dias["miercoles"] + $dias["jueves"] + $dias["viernes"])/5;
 
     echo "las horas por semana son: ";
     echo $horasPorsemana;

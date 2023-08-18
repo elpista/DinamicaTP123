2693431b4559fe55edfc74e81940c30dfada5d6e
Marco pistagnesi <espadachin2003@gmail.com>
2023-08-18 16:09:12 -0300

    ejercicio 3 hecho

---
+++ TP1/EJ3GET/servidor.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hola, yo soy " . $_GET["form_nombre"] . ", " . $_GET["form_apellido"] . " tengo " . $_GET["form_edad"] . 
+" anios y vivo en " . $_GET["form_direccion"];
+?>
---
+++ TP1/EJ3POST/servidor.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hola, yo soy " . $_POST["form_nombre"] . ", " . $_POST["form_apellido"] . " tengo " . $_POST["form_edad"] . 
+" anios y vivo en " . $_POST["form_direccion"];
+?>

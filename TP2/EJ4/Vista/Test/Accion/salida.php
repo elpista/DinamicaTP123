d9d155e055d4c69512f4cd945ea081cb2231ebee
ThomasRifo <thomasmrifo@gmail.com>
2023-08-24 17:52:03 -0300

    Arreglos TP2 EJ4

--- TP2/EJ4/Vista/Test/Accion/salida.php
+++ TP2/EJ4/Vista/Test/Accion/salida.php
@@ -23,7 +23,7 @@ transform: translate(-50%, -50%);" class="contenedorSalida">
 <h2 class="titulo" style="color: #4d2df7; display:inline-block; font-family: arial; font-size: 1.6em; margin: 1.8em 0.8em 1.0em;">La película introducida es</h2>
 <a style="display:inline; text-decoration: none;position: fixed;
 top: 1.2em;
-right: .8em; font-size: 1.2em; font-family: arial; color: grey; " href="../Vista/Test/index.php">X</a>
+right: .8em; font-size: 1.2em; font-family: arial; color: grey; " href="../index.php">X</a>
 <div style="color:green; margin-left: 1em; font-size: 1.2em; font-family: arial; ;"class="datos">
 <p style="margin: 0.4em"><strong>Título:</strong> <?php echo $_POST["tituloInput"] ?></p>
 <p style="margin: 0.4em"><strong>Actores:</strong> <?php echo $_POST["actores"] ?>

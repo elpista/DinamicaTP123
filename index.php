f0c915c228758a471f8ce22a04aaaf29c896fb7f
Marco pistagnesi <espadachin2003@gmail.com>
2023-08-24 20:10:00 -0300

    menu no terminado

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title> Menu principal </title>
+    </head>
+    <body>
+        <h1>
+            Trabajo practico 1: 
+        </h1>
+        <br/>
+        <button type="button" onclick="location.href='TP1/EJ1/Vista/index.php'">EJ1</button>
+        <button type="button" onclick="location.href='TP1/EJ2/Vista/index.php'">EJ2</button>
+        <button type="button" onclick="location.href='TP1/EJ3/Vista/index.php'">EJ3</button>
+        <br/>
+
+        <h1>
+            Trabajo practico 2:
+        </h1>
+        <br/>
+        <button type="button" onclick="location.href='TP2/EJ1/Vista/index.php'">EJ1</button>
+        <button type="button" onclick="location.href='TP2/EJ2/Vista/index.php'">EJ2</button>
+        <button type="button" onclick="location.href='TP2/EJ3/Vista/index.php'">EJ3</button>
+        <button type="button" onclick="location.href='TP2/EJ4/Vista/index.php'">EJ4</button>
+        <br/>
+
+        <h1> 
+            Trabajo practico 3:
+        </h1>
+        <br/>
+        <button type="button" onclick="location.href='TP3/EJ1/Vista/index.php'">EJ1</button>
+        <button type="button" onclick="location.href='TP3/EJ2/Vista/index.php'">EJ2</button>
+        <button type="button" onclick="location.href='TP3/EJ3/Vista/index.php'">EJ3</button>
+    </body>
+</html>

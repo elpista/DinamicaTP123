3718f248fdcbd64624280f4fc438170ae97c179e
Marco pistagnesi <espadachin2003@gmail.com>
2023-08-24 21:22:49 -0300

    menu actualizado

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         <button type="button" onclick="location.href='TP2/EJ1/Vista/index.php'">EJ1</button>
         <button type="button" onclick="location.href='TP2/EJ2/Vista/index.php'">EJ2</button>
         <button type="button" onclick="location.href='TP2/EJ3/Vista/index.php'">EJ3</button>
-        <button type="button" onclick="location.href='TP2/EJ4/Vista/index.php'">EJ4</button>
+        <button type="button" onclick="location.href='TP2/EJ4/Vista/Test/index.php'">EJ4</button>
         <br/>
 
         <h1> 
@@ -29,6 +29,6 @@
         <br/>
         <button type="button" onclick="location.href='TP3/EJ1/Vista/index.php'">EJ1</button>
         <button type="button" onclick="location.href='TP3/EJ2/Vista/index.php'">EJ2</button>
-        <button type="button" onclick="location.href='TP3/EJ3/Vista/index.php'">EJ3</button>
+        <button type="button" onclick="location.href='TP3/EJ3/Vista/Test/index.php'">EJ3</button>
     </body>
 </html>

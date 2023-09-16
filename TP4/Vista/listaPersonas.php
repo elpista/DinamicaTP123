883df8896e5167aaab19dc9915db560a465c99ec
Julian Blanco <116972020+juliaanbl@users.noreply.github.com>
2023-09-15 22:38:08 -0300

    Update de estilo a listaPersonas.php

--- TP4/Vista/listaPersonas.php
+++ TP4/Vista/listaPersonas.php
@@ -1,5 +1,6 @@
 <?php
     include_once("../configuracion.php");
+    include_once '../estructura.php';
     $abmPersona= new AbmPersona();
     $personas = array();
     $personas = $abmPersona->buscar(null);
@@ -13,38 +14,42 @@
     <title>Lista Personas</title>
 </head>
 <body>
-    <div class="contenido">
-        <h1>Lista de Personas</h1>
-        <div>
-            <table>
-                <tr>
-                    <th>DNI</th>
-                    <th>Apellido</th>
-                    <th>Nombre</th>
-                    <th>Fecha Nacimiento</th>
-                    <th>Teléfono</th>
-                    <th>Domicilio</th>
-                    <th>Autos</th>
-                </tr>
+    <div class="container mt-5">
+        <h1 class="text-center">Lista de Personas</h1>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>DNI</th>
+                        <th>Apellido</th>
+                        <th>Nombre</th>
+                        <th>Fecha Nacimiento</th>
+                        <th>Teléfono</th>
+                        <th>Domicilio</th>
+                        <th>Autos</th>
+                    </tr>
+                </thead>
                 <tbody>
                     <?php
                     if (count($personas) > 0) {
-                        for ($i = 0; $i < count($personas); $i++) {
+                        foreach ($personas as $persona) {
                             echo "<tr>";
-                            echo "<td>" . $personas[$i]->getNroDni() . "</td>";
-                            echo "<td>" . $personas[$i]->getApellido() . "</td>";
-                            echo "<td>" . $personas[$i]->getNombre() . "</td>";
-                            echo "<td>" . $personas[$i]->getFechaNac() . "</td>";
-                            echo "<td>" . $personas[$i]->getTelefono() . "</td>";
-                            echo "<td>" . $personas[$i]->getDomicilio() . "</td>";
-                            echo '<td><form action="./accion/autosPersona.php" method="post">
-                                    <input type="hidden" name="NroDni" value="' . $personas[$i]->getNroDni() . '">
-                                    <button type="submit" class="btn btn-link">Ver Autos</button>
-                                    </form></td>';
+                            echo "<td>" . $persona->getNroDni() . "</td>";
+                            echo "<td>" . $persona->getApellido() . "</td>";
+                            echo "<td>" . $persona->getNombre() . "</td>";
+                            echo "<td>" . $persona->getFechaNac() . "</td>";
+                            echo "<td>" . $persona->getTelefono() . "</td>";
+                            echo "<td>" . $persona->getDomicilio() . "</td>";
+                            echo '<td>
+                                    <form action="./accion/autosPersona.php" method="post">
+                                        <input type="hidden" name="NroDni" value="' . $persona->getNroDni() . '">
+                                        <button type="submit" class="btn btn-link">Ver Autos</button>
+                                    </form>
+                                </td>';
                             echo "</tr>";
                         }
                     } else {
-                        echo "<tr><td>No hay Personas</td></tr>";
+                        echo "<tr><td colspan='7'>No hay Personas</td></tr>";
                     }
                     ?>
                 </tbody>

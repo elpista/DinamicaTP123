10acff0ac1dd794df5021d75f98ad04b24cc94e4
Marco pistagnesi <espadachin2003@gmail.com>
2023-10-06 20:42:26 -0300

    tabla google agregada al sql

---
+++ TPClasesUtiles/cuentas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `google`(
+    `email` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
+    `name` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
+    `picture` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
+    PRIMARY KEY  (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+

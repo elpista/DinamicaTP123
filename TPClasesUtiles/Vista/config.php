6f531166a67f2665c47364812d14226a9587ea4e
Marco pistagnesi <espadachin2003@gmail.com>
2023-10-12 16:45:36 -0300

    Mover archivos a la vista

--- TPClasesUtiles/Vista/config.php
+++ TPClasesUtiles/Vista/config.php
@@ -6,7 +6,7 @@ session_start();
 //config.php
 
 //Include Google Client Library for PHP autoload file
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
@@ -18,7 +18,7 @@ $google_client->setClientId('691743210094-abn79b0i03sbfo3johlstqcuck58r5bk.apps.
 $google_client->setClientSecret('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');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost/DinamicaTP123/TPClasesUtiles/index.php');
+$google_client->setRedirectUri('http://localhost/DinamicaTP123/TPClasesUtiles/Vista/index.php');
 
 // to get the email and profile 
 $google_client->addScope('email');

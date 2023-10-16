c68142f69aeba9ed98a55986520e0c3d8d623b3e
Julian Blanco <116972020+juliaanbl@users.noreply.github.com>
2023-10-16 20:16:19 -0300

    subo parte del abmGoogle.php

---
+++ TPClasesUtiles/Control/abmGoogle.php
@@ -0,0 +1,50 @@
+<?php
+class AbmGoogle{
+    
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves coinciden con los nombres de las variables instancias del objeto
+     * @param array $param
+     * @return Auto
+     */
+    private function cargarObjeto($param){
+        $obj = null;      
+        if (array_key_exists('email',$param) && array_key_exists('name',$param) && array_key_exists('picture',$param)){
+            $obj = new Google();
+            $obj->setear($param['email'], $param['name'], $param['picture']);
+        }
+        return $obj;
+    }
+    
+    /**
+     * 
+     * @param array $param
+     */
+    public function alta($param){
+        $resp = false;
+        $objGoogle = $this->cargarObjeto($param);
+        if ($objGoogle!=null && $objGoogle->insertar()){
+            $resp = true;
+        }
+        return $resp;
+        
+    }
+
+    /**
+     * permite buscar un objeto
+     * @param array $param
+     * @return boolean
+     */
+    public function buscar($param){
+        $where = " true ";
+        if ($param<>NULL){
+            if(isset($param['email'])) $where.=" and email = '".$param['email']."'";
+            if(isset($param['name'])) $where.=" and name ='".$param['name']."'";
+            if(isset($param['picture'])) $where.=" and picture ='".$param['picture']."'";
+        }
+
+        $objGoogle = new Google;
+        $arreglo = $objGoogle->listar($where);
+
+        return $arreglo;
+    }
+}

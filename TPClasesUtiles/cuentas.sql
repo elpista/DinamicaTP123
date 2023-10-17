c3f88151ed06d4b59a4adaadc0f8fcb4cbe8120a
Marco pistagnesi <espadachin2003@gmail.com>
2023-10-17 16:40:46 -0300

    relacion de sql agregada

--- TPClasesUtiles/cuentas.sql
+++ TPClasesUtiles/cuentas.sql
@@ -9,5 +9,9 @@ CREATE TABLE `steam`(
     `steamid` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
     `personaname` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
     `avatarmedium` varchar(100) character set utf8 collate utf8_unicode_ci NOT NULL,
+    `googleEmail` varchar(50) character set utf8 collate utf8_unicode_ci NOT NULL,
     PRIMARY KEY (`steamid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `steam`
+ADD CONSTRAINT `steam_ibfk_1` FOREIGN KEY (`googleEmail`) REFERENCES `google` (`email`);
